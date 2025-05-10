a672b19bdf8471b2fc3b1846f575cb504d428ada
TomTom2k <tramthu96@gmail.com>
2025-05-11 00:29:45 +0700

    Update contact email address in Contact component for direct communication.

--- components/contact.tsx
+++ components/contact.tsx
@@ -32,7 +32,7 @@ export default function Contact() {
 
 			<p className='text-gray-700 -mt-6 dark:text-white/80'>
 				Please contact me directly at{' '}
-				<a className='underline' href='mailto:example@gmail.com'>
+				<a className='underline' href='mailto:ngthanhtin68@gmail.com'>
 					ngthanhtin68@gmail.com
 				</a>{' '}
 				or through this form.
