e42ca6c2f2ff516916ede1a8f8e2717915e33729
TomTom2k <tramthu96@gmail.com>
2025-05-11 00:02:11 +0700

    Update experience and project components for improved performance and consistency. Switched to dynamic imports for the VerticalTimeline component and adjusted JSX structure in the Project component for better readability.

--- components/project.tsx
+++ components/project.tsx
@@ -34,13 +34,15 @@ export default function Project({
 			<section className='bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20'>
 				<div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]'>
 					<h3 className='text-2xl font-semibold'>
-						{title}{' '}
-						<a
-							href={link}
-							target='_blank'
-							rel='noopener noreferrer'>
-							<FaExternalLinkAlt className='inline-block ml-2 h-3 w-3' />
-						</a>
+						{title}
+						<span>
+							<a
+								href={link}
+								target='_blank'
+								rel='noopener noreferrer'>
+								<FaExternalLinkAlt className='inline-block ml-2 h-3 w-3' />
+							</a>
+						</span>
 					</h3>
 					<ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
 						{tags.map((tag, index) => (
