bbfdc462b273c253cd6fbcc3cbfd59baefbf07e3
TomTom2k <tramthu96@gmail.com>
2025-05-10 23:48:57 +0700

    Add yarn.lock file and update various components for consistency and improved functionality. Adjusted email sending logic, modified layout and project details, and added new images for better presentation.

--- lib/data.ts
+++ lib/data.ts
@@ -1,107 +1,92 @@
-import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import React from 'react';
+import { CgWorkAlt } from 'react-icons/cg';
+import { FaReact } from 'react-icons/fa';
+import { LuGraduationCap } from 'react-icons/lu';
+import vnmusicImg from '@/public/vnmusic.png';
+import aiStudioImg from '@/public/ai-studio.png';
 
 export const links = [
-  {
-    name: "Home",
-    hash: "#home",
-  },
-  {
-    name: "About",
-    hash: "#about",
-  },
-  {
-    name: "Projects",
-    hash: "#projects",
-  },
-  {
-    name: "Skills",
-    hash: "#skills",
-  },
-  {
-    name: "Experience",
-    hash: "#experience",
-  },
-  {
-    name: "Contact",
-    hash: "#contact",
-  },
+	{
+		name: 'Home',
+		hash: '#home',
+	},
+	{
+		name: 'About',
+		hash: '#about',
+	},
+	{
+		name: 'Projects',
+		hash: '#projects',
+	},
+	{
+		name: 'Skills',
+		hash: '#skills',
+	},
+	{
+		name: 'Experience',
+		hash: '#experience',
+	},
+	{
+		name: 'Contact',
+		hash: '#contact',
+	},
 ] as const;
 
 export const experiencesData = [
-  {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
-    description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
-  },
-  {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
-  },
+	{
+		title: 'TANCA.io - Front-End Developer',
+		location: 'Ho Chi Minh, Vietnam',
+		description:
+			'I worked as a web and mobile developer using React and React Native, gaining my first hands-on experience in real projects. I developed key features like news feed, calendar, digital signature, and information hub, and integrated with Bitrix24. One major challenge was balancing new feature development while fixing bugs reported by customer support, which helped sharpen my problem-solving skills. I collaborated closely with designers, backend developers, and clients, using tools like Postman, Figma, and GitLab.',
+		icon: React.createElement(FaReact),
+		date: '8/2023 - Now',
+	},
+	{
+		title: 'CMC Media - Front-End Developer',
+		location: 'Ha Noi, Vietnam',
+		description:
+			'At CMC Media, I worked mainly with PHP Laravel, stepping out of my React comfort zone. My tasks included fixing bugs, updating outdated layouts, implementing multi-language support, and ensuring responsive design. Initially, understanding the API structure for debugging was challenging, but my AI-assisted learning skills helped me quickly adapt to the new tech stack. I worked closely with designers, backend developers, and clients to clarify requirements, and used Postman, Figma, and GitLab for daily tasks.',
+		icon: React.createElement(CgWorkAlt),
+		date: '2/2025 - Now',
+	},
 ] as const;
 
 export const projectsData = [
-  {
-    title: "CorpComment",
-    description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
-  },
-  {
-    title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
-  },
+	{
+		title: 'VNMusic',
+		description:
+			'I worked as a mobile developer on the VNMusic app for 3 months. The app lets users stream music, create playlists, and get notifications about new releases, similar to Spotify. I focused on building core music streaming and user interaction features to ensure a smooth experien',
+		tags: ['React Native', 'TypeScript', 'One Signal', 'Redux'],
+		imageUrl: vnmusicImg,
+		link: 'https://drive.google.com/drive/folders/1Zaw7i-p-gUc6Xx7sR1rie6Q_Gm_oIl3P?usp=sharing',
+	},
+	{
+		title: 'Automation Tool Studio',
+		description:
+			'I worked on a project that converts user prompts into stories, generating characters, images, videos, sound, and voiceovers to create short videos. I focused on building a responsive, user-friendly interface for mobile and desktop, with dark/light modes and multi-language support for better accessibility.',
+		tags: ['NextJs', 'Shadcn', 'Tailwind', 'i18n', 'websockets'],
+		imageUrl: aiStudioImg,
+		link: 'https://drive.google.com/drive/folders/1kXY7FVsGotmquPcAUZv20uUY6Zs83s-e?usp=sharing',
+	},
 ] as const;
 
 export const skillsData = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "TypeScript",
-  "React",
-  "Next.js",
-  "Node.js",
-  "Git",
-  "Tailwind",
-  "Prisma",
-  "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
-  "PostgreSQL",
-  "Python",
-  "Django",
-  "Framer Motion",
+	'HTML',
+	'CSS',
+	'JavaScript',
+	'TypeScript',
+	'React',
+	'Next.js',
+	'Node.js',
+	'Git',
+	'Tailwind',
+	'Socket.io',
+	'MongoDB',
+	'Redux',
+	'Nestjs',
+	'React Native',
+	'Express',
+	'PostgreSQL',
+	'Python',
+	'Django',
 ] as const;
